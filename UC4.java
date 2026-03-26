45adf4f7660e2baf5f135a0076c4718bb5341385
Srinivas7468 <pp4730@srmist.edu.in>
2026-03-26 13:48:12 +0530

    UC4

--- UC4.java
+++ UC4.java
@@ -1,13 +1,21 @@
-public class UC4{
+public class UC4 {
     public static void main(String[] args) {
-        String names;
+
+        StringBuilder names = new StringBuilder();
 
         if (args.length > 0) {
-            names = String.join(", ", args);
-        } else {
-            names = "World";
-        }
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
 
-        System.out.println("Hello, " + names + "!");
+                // Add comma only if not last element
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+
+            System.out.println("Hello, " + names.toString() + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
